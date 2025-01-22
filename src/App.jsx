beeb0be15a2b3c7c75041e32eb4639b75c2700b3
deepakjain06 <deepakjain0621@gmail.com>
2025-01-22 22:11:22 +0530

    First ever COmmit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import djbutton from "./djbutton";
+
+function App(){
+return 
+<div>
+
+<h1>this is the beat react code     
+</h1>
+
+
+<button>JS example </button>
+<djbutton>
+
+</djbutton>
+</div>
+
+}
+
+export default App;
---
+++ src/djbutton.jsx
@@ -0,0 +1,7 @@
+function djbutton(){
+    return <h1>
+    giveing the example of the code
+    </h1>
+}
+
+export default djbutton;
